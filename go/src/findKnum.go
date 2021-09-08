7974670d7a5917c351b5d56d31df6029bd19422b
werkki96 <werkki96@gmail.com>
2021-09-08 19:36:03 +0900

    0908 K번째수 찾기 <br>\n 자체 내장 sort함수사용 <br>\n go lang은 변수a := 매개변수a` 형태로 변수초기화 시키면 a변수 데이터 변경시 a`의 포인터로 접근해서 변경되니까 copy함수 이용해야함

---
+++ go/src/findKnum.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func test_fknum() {
+	array := []int{1, 5, 2, 6, 3, 7, 4}
+	commands := [][]int{{2, 5, 3}, {4, 4, 1}, {1, 7, 3}}
+
+	fmt.Print(finKnum(array, commands))
+}
+
+func finKnum(array []int, commands [][]int) []int {
+	answer := []int{}
+	for _, s := range commands {
+		arrK := make([]int, s[1]-(s[0]-1))
+		copy(arrK, array[s[0]-1:s[1]])
+		sort.Ints(arrK)
+		answer = append(answer, arrK[s[2]-1])
+	}
+	return answer
+}
--- go/src/main.go
+++ go/src/main.go
@@ -3,4 +3,5 @@ package main
 func main() {
 	testSQRT()
 	testEngToNum()
+	test_fknum()
 }
